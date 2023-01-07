4741c5132240e757d80fcd35c5b434e5911c4de3
Josiel Faria <developerjosiel@gmail.com>
2023-01-07 20:08:19 -0300

    Criando telas para exibição de dados

--- src/components/TopStudiosTable/index.tsx
+++ src/components/TopStudiosTable/index.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonGrid, IonRow } from '@ionic/react';
+import { TopStudios } from '../../types/top-studios.type';
 import './styled.css'
 
-interface TopStudiosTableProps { }
+interface TopStudiosTableProps {
+  data: TopStudios | null,
+}
 
-interface TopStudiosTableState { }
+const TopStudiosTable: React.FC<TopStudiosTableProps> = ({ data }) => {
 
-class TopStudiosTable extends React.Component<TopStudiosTableProps, TopStudiosTableState> {
-  render() {
-    return ('');
-  }
+  return (
+    <IonCard>
+      <IonCardHeader>
+        <IonCardTitle>Top 3 studios with winners</IonCardTitle>
+      </IonCardHeader>
+
+      <IonCardContent>
+        <IonGrid>
+          <IonRow color="success">
+            <IonCol class="ion-text-center" size-xs="6" size-md="6">Name</IonCol>
+            <IonCol class="ion-text-center" size-xs="6" size-md="6">Win Count</IonCol>
+          </IonRow>
+
+          {data && data.studios.slice(0, 3).map((item: any, index: any) => {
+            return (
+              <IonRow>
+                <IonCol size-xs="6" size-md="6">{item.name}</IonCol>
+                <IonCol size-xs="6" size-md="6">{item.winCount}</IonCol>
+              </IonRow>
+            )
+          })}
+        </IonGrid>
+      </IonCardContent>
+    </IonCard >
+  );
 }
 
 export default TopStudiosTable;
--- src/components/YearsMultipleWinnersTable/index.tsx
+++ src/components/YearsMultipleWinnersTable/index.tsx
@@ -1,14 +1,40 @@
 import React from 'react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonGrid, IonRow } from '@ionic/react';
+import { YearsMultipleWinners } from '../../types/years-multiple-winners';
 import './styled.css'
 
-interface YearsMultipleWinnersTableProps { }
+interface YearsMultipleWinnersTableProps {
+  data: YearsMultipleWinners | null,
+}
 
-interface YearsMultipleWinnersTableState { }
+const YearsMultipleWinnersTable: React.FC<YearsMultipleWinnersTableProps> = ({ data }) => {
 
-class YearsMultipleWinnersTable extends React.Component<YearsMultipleWinnersTableProps, YearsMultipleWinnersTableState> {
-  render() {
-    return ('');
-  }
+  return (
+    <IonCard>
+      <IonCardHeader>
+        <IonCardTitle>List years with multiple winners</IonCardTitle>
+      </IonCardHeader>
+
+      <IonCardContent>
+        <IonGrid>
+          <IonRow color="success">
+            <IonCol class="ion-text-center" size-xs="6" size-md="6">Year</IonCol>
+            <IonCol class="ion-text-center" size-xs="6" size-md="6">Win Count</IonCol>
+
+          </IonRow>
+
+          {data && data.years.map((item: any, index: any) => {
+            return (
+              <IonRow>
+                <IonCol size-xs="6" size-md="6">{item.year}</IonCol>
+                <IonCol size-xs="6" size-md="6">{item.winnerCount}</IonCol>
+              </IonRow>
+            )
+          })}
+        </IonGrid>
+      </IonCardContent>
+    </IonCard >
+  );
 }
 
 export default YearsMultipleWinnersTable;

b3336aa3e30654f9d79a25a7b79639e0763040b0
Josiel Faria <developerjosiel@gmail.com>
2023-01-08 12:05:23 -0300

    Adicionando style global

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,11 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+
+/* CSS */
+import './styles/main.css';
+
+/* Pages */
 import DashboardPage from './pages/Dashboard';
 import MovieListPage from './pages/MovieList';
 

cc94b36f1c06727702d3fb560a7f2e8f6c5c1c33
Josiel Faria <developerjosiel@gmail.com>
2023-01-12 16:40:59 -0300

    Ajuste location

--- src/pages/Dashboard/index.test.tsx
+++ src/pages/Dashboard/index.test.tsx
@@ -6,7 +6,18 @@ import YearsMultipleWinnersTable from '../../components/YearsMultipleWinnersTabl
 import TopStudiosTable from '../../components/TopStudiosTable';
 import ProducersIntervalTable from '../../components/ProducersIntervalTable';
 
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useLocation: () => ({
+    pathname: 'localhost:8100/page/dashboard',
+  }),
+}));
+
 describe('DashboardPage', () => {
+  const component = <DashboardPage />;
+  const wrapper = shallow(component);
+  render(<DashboardPage />);
+
   test('render page', () => {
     const { baseElement } = render(<DashboardPage />);
     expect(baseElement).toBeDefined();
@@ -15,27 +26,23 @@ describe('DashboardPage', () => {
   test('there is a title on the ion page', () => {
     render(<DashboardPage />);
     const title = screen.getByTestId('ion-page');
-    const titleDashboard = within(title).getAllByText('Dashboard')
+    const titleDashboard = within(title).getAllByText('Dashboard');
     expect(titleDashboard).toBeTruthy();
-  })
+  });
 
   it('render component YearsMultipleWinnersTable', () => {
-    const wrapper = shallow(<DashboardPage />);
     expect(wrapper.find(YearsMultipleWinnersTable)).toHaveLength(1);
   });
 
   it('render component TopStudiosTable', () => {
-    const wrapper = shallow(<DashboardPage />);
     expect(wrapper.find(TopStudiosTable)).toHaveLength(1);
   });
 
   it('render component ProducersIntervalTable', () => {
-    const wrapper = shallow(<DashboardPage />);
     expect(wrapper.find(ProducersIntervalTable)).toHaveLength(1);
   });
 
   it('render component WinnersByYearTable', () => {
-    const wrapper = shallow(<DashboardPage />);
     expect(wrapper.find(WinnersByYearTable)).toHaveLength(1);
   });
 });

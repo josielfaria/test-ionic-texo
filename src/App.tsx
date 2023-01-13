e464b3b1f108b5a86d981447ff043fd59137b68f
Josiel Faria <developerjosiel@gmail.com>
2023-01-13 09:50:47 -0300

    Finish deploy config

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ const App: React.FC = () => {
         <IonSplitPane contentId='main'>
           <IonReactHashRouter >
             <IonRouterOutlet id='main'>
-              <Route path='/'>
+              <Route path=''>
                 <Redirect to='/page/dashboard' />
               </Route>
 
